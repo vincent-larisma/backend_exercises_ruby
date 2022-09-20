d3ea7efbc51d7693512b8950b5716becc47a3e79
Vincy <vincentangelo.larisma@gmail.com>
2022-09-20 21:26:27 +0800

    done with exercise

---
+++ Activity 3/activity3.rb
@@ -0,0 +1,16 @@
+
+puts "Check if it is a perfect square or not!"
+input_number = gets.chomp.to_i
+
+
+
+def check_if_perfect_square(number)
+    for x in (0..number) do
+        if (x * x) == number 
+            return puts "#{number} is a perfect square"
+        elsif (x * x) > number
+            return puts "#{number} is not a perfect square"
+        end
+    end
+end
+check_if_perfect_square(input_number)
