cbe47504d8fd0fc9345e5619e9a14b6fee32ca02
Vincy <103049779+vincent-larisma@users.noreply.github.com>
2022-09-22 19:56:50 +0800

    Done with activity3

--- Activity 3/activity3.rb
+++ Activity 3/activity3.rb
@@ -2,15 +2,18 @@
 puts "Check if it is a perfect square or not!"
 input_number = gets.chomp.to_i
 
-
-
 def check_if_perfect_square(number)
     for x in (0..number) do
-        if (x * x) == number 
+        if (x * x) == number
             return puts "#{number} is a perfect square"
-        elsif (x * x) > number
-            return puts "#{number} is not a perfect square"
         end
     end
+    puts "#{number} is not a perfect square"
 end
+
 check_if_perfect_square(input_number)
+
+
+
+
+
